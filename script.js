01a2e0e648105822f6e9054dfd832192ae800d76
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-11-18 11:58:20 +0800

    Started dark mode on home

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const printHoverElem = document.querySelector('.print-hover');
 const reportHoverElem = document.querySelector('.user-report-hover');
+const bodyElem = document.querySelector('body');
+const headerLeftH1 = document.querySelector('.header-left h1');
+const headerRightH1 = document.querySelector('.header-right a');
+const menuAElems = document.querySelectorAll('.menu a');
 
 function printHover(){
     printHoverElem.style.visibility = 'visible';
@@ -15,4 +19,15 @@ function reportHover(){
 
 function reportHoverOut(){
     reportHoverElem.style.visibility = 'hidden';
+}
+
+function switchMode(){
+    bodyElem.style.background = '#1B1919'; // rgba(217, 217, 217, 0.25) does not have the same effect
+
+    headerLeftH1.style.color = 'white';
+    headerRightH1.style.color = 'white';
+
+    menuAElems.forEach(menuAElem => {
+        menuAElem.style.color = 'white';
+    });     
 }
