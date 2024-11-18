dde3ac5f9f7adf39d6423d7091ea60687b36e8e3
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-11-18 08:13:43 +0800

    Index foundation

---
+++ script.js
@@ -0,0 +1,18 @@
+const printHoverElem = document.querySelector('.print-hover');
+const reportHoverElem = document.querySelector('.user-report-hover');
+
+function printHover(){
+    printHoverElem.style.visibility = 'visible';
+}
+
+function printHoverOut(){
+    printHoverElem.style.visibility = 'hidden';
+}
+
+function reportHover(){
+    reportHoverElem.style.visibility = 'visible';
+}
+
+function reportHoverOut(){
+    reportHoverElem.style.visibility = 'hidden';
+}
