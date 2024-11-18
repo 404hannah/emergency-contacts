4d76f8b002141aa896727ff25da87bcf48db9555
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-11-18 16:20:10 +0800

    Menu button and switch modes

--- script.js
+++ script.js
@@ -1,33 +1,75 @@
-const printHoverElem = document.querySelector('.print-hover');
-const reportHoverElem = document.querySelector('.user-report-hover');
-const bodyElem = document.querySelector('body');
-const headerLeftH1 = document.querySelector('.header-left h1');
-const headerRightH1 = document.querySelector('.header-right a');
-const menuAElems = document.querySelectorAll('.menu a');
-
 function printHover(){
+    const printHoverElem = document.querySelector('.print-hover');
     printHoverElem.style.visibility = 'visible';
 }
 
 function printHoverOut(){
+    const printHoverElem = document.querySelector('.print-hover');
     printHoverElem.style.visibility = 'hidden';
 }
 
 function reportHover(){
+    const reportHoverElem = document.querySelector('.user-report-hover');
     reportHoverElem.style.visibility = 'visible';
 }
 
 function reportHoverOut(){
+    const reportHoverElem = document.querySelector('.user-report-hover');
     reportHoverElem.style.visibility = 'hidden';
 }
 
 function switchMode(){
-    bodyElem.style.background = '#1B1919'; // rgba(217, 217, 217, 0.25) does not have the same effect
+    const bxModeId = document.getElementById('bx-mode');
+    const bodyElem = document.querySelector('body');
+    const headerLeftH1 = document.querySelector('.header-left h1');
+    const headerRightA = document.querySelector('.header-right a');
+    const printElem = document.querySelector('.print');
+    const reportElem = document.querySelector('.user-report');
 
-    headerLeftH1.style.color = 'white';
-    headerRightH1.style.color = 'white';
+    const menuAElems = document.querySelectorAll('.menu a');
 
-    menuAElems.forEach(menuAElem => {
-        menuAElem.style.color = 'white';
-    });     
+    if(bxModeId.classList == 'bx bxs-moon bx-lg'){
+        bxModeId.classList.remove("bxs-moon", "bx-lg");
+        bxModeId.classList.add("bx-sun", "bx-lg");
+
+        bodyElem.style.background = '#1B1919'; // rgba(217, 217, 217, 0.25) does not have the same effect
+
+        headerLeftH1.style.color = 'white';
+        headerRightA.style.color = 'white';
+
+        menuAElems.forEach(menuAElem => {
+            menuAElem.style.color = 'white';
+        });     
+
+        printElem.style.borderColor = '#959595';
+        reportElem.style.borderColor = '#959595';
+    } else {
+        bxModeId.classList.remove("bx-sun", "bx-lg");
+        bxModeId.classList.add("bxs-moon", "bx-lg");
+
+        bodyElem.style.background = '#C7E5F4';
+
+        headerLeftH1.style.color = '#0B0B0B';
+        headerRightA.style.color = '#0B0B0B';
+
+        menuAElems.forEach(menuAElem => {
+            menuAElem.style.color = '#0B0B0B';
+        });     
+
+        printElem.style.borderColor = '#2F3030';
+        reportElem.style.borderColor = '#2F3030';
+    }
+}
+
+function menu(){
+    const menuElem = document.querySelector('.menu-wrapper');
+    const menuBtn = document.querySelector('.menu-button');
+
+    if(menuBtn.style.left == '0%'){
+        menuElem.style.visibility = 'visible';
+        menuBtn.style.left = '28%';
+    } else {
+        menuElem.style.visibility = 'hidden';
+        menuBtn.style.left = '0%';
+    }
 }
