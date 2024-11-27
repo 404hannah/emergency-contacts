e59ee29d426101c7dabef116f66cd0dba427ac39
Hannah Pulmano <hannah.pulmano865@gmail.com>
2024-11-27 10:41:49 +0800

    fixed tabs

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,21 +1,20 @@
 function viewMode(){
     const view = document.querySelector('.container-view');
     const manage = document.querySelector('.container-manage');
-    const form = document.querySelector('form');
+    const subform = document.querySelector('.sub-form');
     
     manage.style.display = 'none';
     view.style.display = 'flex';
-    form.style.backgroundColor = '#9B9B9B';
+    subform.style.backgroundColor = '#9B9B9B';
     
 }
 
 function manageMode(){
     const view = document.querySelector('.container-view');
     const manage = document.querySelector('.container-manage');
-    const form = document.querySelector('form');
+    const subform = document.querySelector('.sub-form');
     
     manage.style.display = 'flex';
     view.style.display = 'none';
-    form.style.backgroundColor = '#728F90';
-    
+    subform.style.backgroundColor = '#728F90';
 }
